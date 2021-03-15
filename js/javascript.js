4082deceedb1170d68f77622b14bacbb8390f6a6
max-frogemar-nti-johanneberg <max.frogemar@elev.ga.ntig.se>
2021-03-15 10:49:31 +0100

    nav
    nav 100% klart

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,5 @@
 function toggleMeny() {
+    document.querySelector('.nav-block-element').classList.toggle('nav-block-element-active');
     document.querySelector('.nav-links').classList.toggle('nav-active');
     document.querySelector('.hamburgare').classList.toggle('toggle');
 }
